8578b991a52675f8ee572676963c46f82370929d
tadanobutaaaaa <youyah1004@gmail.com>
2024-12-30 20:15:21 +0900

    helpページの削除、それに伴う修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import SetupProcess from "./pages/setup_process"
 import Manual from "./pages/manual"
-import Help from "./pages/help"
 
 function App() {
     return (
@@ -11,7 +10,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/setup_process" element={<SetupProcess/>}/>
             <Route path="/manual" element={<Manual/>}/>
-            <Route path="/help" element={<Help/>}/>
         </Routes>
     );
 }
