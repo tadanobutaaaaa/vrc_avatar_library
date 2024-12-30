d431bb9dbb436b46a81f693266d873c46cd57f71
tadanobutaaaaa <youyah1004@gmail.com>
2024-12-31 01:13:53 +0900

    ヘッダーにホームアイコンを追加し、見出しサイズを変更

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Flex, HStack, Box, Heading, } from '@chakra-ui/react';
-import { CircleHelp, Info, Settings } from 'lucide-react';
+import { CircleHelp, Info, Settings, House } from 'lucide-react';
 import { BrowserOpenURL } from "../../wailsjs/runtime/runtime";
 import { IconButton } from "@chakra-ui/react"; 
 import { useNavigate } from "react-router-dom";
@@ -8,11 +8,21 @@ import Page from './coloModeButton';
 function Header () {
     const navigate = useNavigate()
     return (
-        <Flex alignItems="center" justifyContent="center" bg="teal.400" p={6} position="relative">
+        <Flex alignItems="center" justifyContent="center" bg="teal.400" p={5} position="relative">
             <Box textAlign="center">
-                <Heading>VRC Avater Library</Heading>
+                <Heading size="2xl">VRC Avater Library</Heading>
             </Box>
             
+            <HStack position="absolute" left={5}>
+                <IconButton
+                    onClick={() => navigate("/") }
+                    variant="ghost"
+                    aria-label='Toggle color mode'
+                >
+                    <House/>
+                </IconButton>
+            </HStack>
+
             <HStack position="absolute" right={5}>
                 <IconButton
                     onClick={() => navigate("/manual")}
