43b325006058aaaeddf42931d8cf7157c6fdfd81
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-14 23:45:51 +0900

    設定ファイルを作成し、選択したフォルダのパスを保存する機能を追加

--- app.go
+++ app.go
@@ -3,9 +3,16 @@ package main
 import (
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 
+	"os"
 	"fmt"
 	"context"
+	"path/filepath"
+	"encoding/json"
 )
+
+type Config struct {
+	SearchFolder string `json:"searchFolder"`
+}
 
 // App struct
 type App struct {
@@ -18,17 +25,65 @@ func NewApp() *App {
 }
 
 func (a *App) startup(ctx context.Context) {
+	if _, err := os.Stat("Config"); os.IsNotExist(err) {
+		if err := os.Mkdir("Config", 0750); err != nil {
+			return
+		}
+	}
+	if _, err := os.Stat("./Config/config.json"); os.IsNotExist(err) {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("ユーザーホームディレクトリを取得できませんでした: ",err)
+		}
+		DownloadPath := filepath.Join(home, "Downloads")
+
+		initialConfig := Config{
+			SearchFolder: DownloadPath,
+		}
+
+		file, err := os.Create("./Config/config.json")
+		if err != nil {
+			fmt.Println("設定ファイルを作成できませんでした: ",err)
+		}
+		defer file.Close()
+
+		encoder := json.NewEncoder(file)
+		encoder.SetIndent("", "  ") // 見やすいようにインデントを設定
+		if err := encoder.Encode(initialConfig); err != nil {
+			fmt.Printf("JSON エンコードエラー: %v\n", err)
+			return
+		}
+	}
 	a.ctx = ctx
 	GoServer()
 }
-func (a *App) SelectFolder() {
+
+func (a *App) SelectFolder() string {
 	result, err := runtime.OpenDirectoryDialog(a.ctx, runtime.OpenDialogOptions{
 		Title: "フォルダを選択",
 	})
 	if err != nil {
 		fmt.Println("フォルダを選択する際にエラーが発生しました: ",err)
 	}	
+
+	initialConfig := Config{
+		SearchFolder: result,
+	}
+
+	file, err := os.Create("./Config/config.json")
+	if err != nil {
+		fmt.Println("設定ファイルを作成できませんでした: ",err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ") // 見やすいようにインデントを設定
+	if err := encoder.Encode(initialConfig); err != nil {
+		fmt.Printf("JSON エンコードエラー: %v\n", err)
+	}
+
 	fmt.Println(result)
+	return result
 }
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
