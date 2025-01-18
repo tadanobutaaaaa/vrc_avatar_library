ac3704e3a1e4de43701b9c8be5535880ed436485
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-18 12:46:42 +0900

    ProcessingおよびResultコンポーネントへのルートを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import SetupProcess from "./pages/setupProcess"
 import Manual from "./pages/manual"
+import Processing from "./pages/processing"
+import Result from "./pages/result"
 
 function App() {
+
+
+
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/setupProcess" element={<SetupProcess/>}/>
             <Route path="/manual" element={<Manual/>}/>
+            <Route path="/processing" element={<Processing/>}/>
+            <Route path="/result" element={<Result/>} />
         </Routes>
     );
 }
