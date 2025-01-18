ed00a1cfdd3691ab0bbfccc810f24db51421c3d7
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-18 12:10:09 +0900

    Homeコンポーネントを追加し、処理状況を定期的に取得する機能を実装。UIを改善し、Flexコンポーネントを使用してレイアウトを調整。

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { BookText, Settings, Folder, CircleHelp } from 'lucide-react';
 import { BrowserOpenURL } from "../../wailsjs/runtime/runtime";
-import { Heading, Center, Box, Text, VStack, Image, Link, Flex } from "@chakra-ui/react"
+import { Heading, Center, Box, Text, Image, Link, Flex, Icon } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import Header from '../components/Header';
-function App(){
+
+function Home(){
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetch("http://localhost:8080/progress", {
+                mode: "cors",
+                method: "GET",
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data.status)
+                    if (data.status === true) {
+                        console.log("処理が開始しています")
+                        clearInterval(interval)
+                        navigate("/processing")
+                        return
+                    }
+                })
+                .catch((error) => {
+                    console.error("処理が開始していません:", error)
+                })
+        }, 2000)
+        return () => clearInterval(interval)
+    }, [])
+
     return (
         <>
             <Header />
@@ -11,43 +38,45 @@ function App(){
                 <BookText size="40px"/>
                 <Heading size="4xl" ml="5px">使い方</Heading>
             </Center>
-            <VStack mt="30px" mb="50px" gap="40px">
-                <Box>
+            <Flex mt="30px" mb="50px" gap="40px" justifyContent="flex-start" flexDirection="column">
+            <Box>
+                <Center>
+                    <Heading>1.Chrome拡張機能をインストールする</Heading>
+                </Center>
+                <Flex ml="100px" mt="15px">
+                    <Image 
+                        src="/images/chromeExtension.png"
+                        h="230px"
+                        borderWidth="1px"
+                        borderColor="gray.300"
+                        borderStyle="solid"
+                        borderRadius="md"
+                    ></Image>
                     <Center>
-                        <Heading>1.Chrome拡張機能をインストールする</Heading>
-                    </Center>
-                    <Center mt="15px">
-                        <Image 
-                            src="/images/chromeExtension.png"
-                            h="230px"
-                            borderWidth="1px"
-                            borderColor="gray.300"
-                            borderStyle="solid"
-                            borderRadius="md"
-                        ></Image>
                         <Text ml="15px">
-                            <Link 
-                                onClick={() => {BrowserOpenURL("https://chromewebstore.google.com/detail/vrc-avatar-library/fhjadoafaejfgjcpafnoaopkpbbjganm?authuser=0&hl=ja")}}
-                                variant="underline"
-                                colorPalette={"blue"}
-                                _hover={{ color: "teal" }}
-                                fontWeight="bold"
-                            >Chromeウェブストア</Link>
-                            にアクセスし、右上の「Chromeに追加」ボタン<br />
-                            をクリックしてインストールしてください。<br />
-                            <br />
-                            対応ブラウザは<strong>Google Chrome</strong>はもちろんのこと、<br />
-                            <strong>Microsoft Edge</strong>、<strong>Brave</strong>などのChromium系でも使用可能です。<br />
-                            <br />
-                            <strong>※Firefoxには対応していませんのでご注意ください。</strong>
+                        <Link 
+                            onClick={() => {BrowserOpenURL("https://chromewebstore.google.com/detail/vrc-avatar-library/fhjadoafaejfgjcpafnoaopkpbbjganm?authuser=0&hl=ja")}}
+                            variant="underline"
+                            colorPalette={"blue"}
+                            _hover={{ color: "teal" }}
+                            fontWeight="bold"
+                        >Chromeウェブストア</Link>
+                        にアクセスし、右上の「Chromeに追加」ボタン<br />
+                        をクリックしてインストールしてください。<br />
+                        <br />
+                        対応ブラウザは<strong>Google Chrome</strong>はもちろんのこと、<br />
+                        <strong>Microsoft Edge</strong>、<strong>Brave</strong>などのChromium系でも使用可能です。<br />
+                        <br />
+                        <strong>※Firefoxには対応していませんのでご注意ください。</strong>
                         </Text>
                     </Center>
-                </Box>
+                </Flex>
+            </Box>
                 <Box>
                     <Center>
                         <Heading>2.設定を確認する</Heading>
                     </Center>
-                    <Center mt="15px">
+                    <Flex ml="100px" mt="15px">
                         <Image
                             src="/images/settingFolderPath.png"
                             h="230px"
@@ -56,22 +85,22 @@ function App(){
                             borderStyle="solid"
                             borderRadius="md"
                         ></Image>
-                        <Text ml="15px">
-                            <Flex>
-                                アプリの右上の<Settings />設定アイコンからBoothで買った商品が<br />
-                            </Flex>
+                        <Center ml="15px">
+                            <Text>
+                            アプリの右上の<Icon ml="3px" mb="4px" mr="1px" fontSize="20px"><Settings /></Icon>設定アイコンからBoothで買った商品が<br />
                             保存されるフォルダを指定してください。<br />
                             <br />
                             初期設定ではダウンロードフォルダが設定されています。<br />
                             <strong>この設定が正しくないとプログラムが動かないのでご注意ください。</strong>
-                        </Text>
-                    </Center>
+                            </Text>
+                        </Center>
+                    </Flex>
                 </Box>
                 <Box>
                     <Center>
                         <Heading>3.Boothのライブラリページに移動する</Heading>
                     </Center>
-                    <Center mt="15px">
+                    <Flex ml="100px" mt="15px">
                         <Image
                             src="/images/boothLibrary.png"
                             h="230px"
@@ -80,30 +109,32 @@ function App(){
                             borderStyle="solid"
                             borderRadius="md"
                         ></Image>
-                        <Text ml="15px">
-                            <Link
-                                onClick={() => {BrowserOpenURL("https://chromewebstore.google.com/detail/vrc-avatar-library/fhjadoafaejfgjcpafnoaopkpbbjganm?authuser=0&hl=ja")}}
-                                variant="underline"
-                                colorPalette={"blue"}
-                                _hover={{ color: "teal" }}
-                                fontWeight="bold"
-                            >Booth</Link>にアクセスし右上のアイコンからライブラリを選択します。<br />   
-                            サムネイルを付与したい商品を選択し、「処理開始」ボタン <br />
-                            を押してください。<br />
-                            <br />
-                            <strong>最初は全てにチェックを付けたままの状態で処理を開始することを<br />
-                            おすすめします。</strong> <br />
-                            <br />
-                            <strong>※このようなボタンがない場合は拡張機能がダウンロードされているか<br />
-                            再度ご確認ください。</strong>
-                        </Text>
-                    </Center>
+                        <Center>
+                            <Text ml="15px">
+                                <Link
+                                    onClick={() => {BrowserOpenURL("https://chromewebstore.google.com/detail/vrc-avatar-library/fhjadoafaejfgjcpafnoaopkpbbjganm?authuser=0&hl=ja")}}
+                                    variant="underline"
+                                    colorPalette={"blue"}
+                                    _hover={{ color: "teal" }}
+                                    fontWeight="bold"
+                                >Booth</Link>にアクセスし右上のアイコンからライブラリを選択します。<br />   
+                                サムネイルを付与したい商品を選択し、「処理開始」ボタン <br />
+                                を押してください。<br />
+                                <br />
+                                <strong>最初は全てにチェックを付けたままの状態で処理を開始することを<br />
+                                おすすめします。</strong> <br />
+                                <br />
+                                <strong>※このようなボタンがない場合は拡張機能がダウンロードされているか<br />
+                                再度ご確認ください。</strong>
+                            </Text>
+                        </Center>
+                    </Flex>
                 </Box>
                 <Box>
                     <Center>
                         <Heading>4.フォルダにサムネイルがついているかの確認</Heading>
                     </Center>
-                    <Center mt="15px">
+                    <Flex ml="100px" mt="15px">
                         <Image
                             src="/images/folderSize.png"
                             h="230px"
@@ -112,10 +143,9 @@ function App(){
                             borderStyle="solid"
                             borderRadius="md"
                         ></Image>
-                        <Text ml="15px">
-                            <Flex>
-                                アプリの右上の<Folder />フォルダアイコンをクリックし、<br />
-                            </Flex>
+                        <Center ml="15px">
+                            <Text>
+                            アプリの右上の<Icon ml="3px" mb="3px" fontSize="20px"><Folder /></Icon>フォルダアイコンをクリックし、<br />
                             フォルダが存在しているか確認をしてください。<br />
                             <br />
                             フォルダの存在を確認できたら、エクスプローラーの表示<br />
@@ -124,14 +154,15 @@ function App(){
                             <br />
                             <strong>※サムネイルが表示されない場合は、反映が遅れているだけですので<br />
                             しばらく待ってから確認してください。</strong>
-                        </Text>
-                    </Center>
+                            </Text>
+                        </Center>
+                    </Flex>
                 </Box>
                 <Box>
                     <Center>
                         <Heading>バグの発見、機能の要望などがある場合</Heading>
                     </Center>
-                    <Center mt="15px">
+                    <Flex ml="100px" mt="15px">
                         <Image
                             src="/images/googleForms.png"
                             h="230px"
@@ -140,22 +171,22 @@ function App(){
                             borderStyle="solid"
                             borderRadius="md"
                         ></Image>
-                        <Text ml="15px">
-                            <Flex>
-                            アプリ右上の<CircleHelp />ヘルプアイコンをクリックし、<br />
-                            </Flex>
-                            グーグルフォームへの記載をお願いします。<br />
-                            <br />
-                            個人開発であるため、対応するまでに時間がかかる場合があります。<br />
-                            ご了承ください。<br />
-                            <br />
-                            <strong>※記載いただいた内容は匿名で送信されますのでご安心ください。</strong>
-                        </Text>
-                    </Center>
+                        <Center ml="15px">
+                            <Text>
+                                アプリ右上の<Icon ml="3px" mb="2px" mr="3px" fontSize="20px"><CircleHelp /></Icon>ヘルプアイコンをクリックし、<br />
+                                グーグルフォームへの記載をお願いします。<br />
+                                <br />
+                                個人開発であるため、対応するまでに時間がかかる場合があります。<br />
+                                ご了承ください。<br />
+                                <br />
+                                <strong>※記載いただいた内容は匿名で送信されますのでご安心ください。</strong>
+                            </Text>
+                        </Center>
+                    </Flex>
                 </Box>
-            </VStack>
+            </Flex>
         </>
     );
 }
 
-export default App
+export default Home
