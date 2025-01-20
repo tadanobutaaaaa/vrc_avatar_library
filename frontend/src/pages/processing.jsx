fdfe832e8eba1444dd7c1f165761a3ffa9d9da94
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-21 02:43:27 +0900

    useWebSocket関数を利用してメッセージ処理を実装し、進捗状況を表示する機能を追加

--- frontend/src/pages/processing.jsx
+++ frontend/src/pages/processing.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import goWebSocket from '../hooks/goWebSocket';
+import React, { useState } from 'react';
+import useWebSocket from '../hooks/goWebSocket';
 import Header from '../components/Header';
-import { Center } from "@chakra-ui/react";
-import { ProgressBar, ProgressRoot, ProgressLabel } from "@/components/ui/progress"
+import { Center, HStack } from "@chakra-ui/react";
+import { ProgressBar, ProgressRoot, ProgressLabel, ProgressValueText } from "@/components/ui/progress"
 
 
 function Processing(){
-    goWebSocket("/result", false)
+    const [processedCount, setProcessedCount] = useState(0)
+
+    const handleMessage = (percentage) => {
+        setProcessedCount(percentage)
+    }
+
+    useWebSocket("/result", false, handleMessage)
 
     return (
         <>
             <Header status={true} />
             <Center h="600px">
-                <ProgressRoot w="300px" value={null}>
+                <ProgressRoot w="300px" value={processedCount} animated>
                     <ProgressLabel info="フォルダにサムネイルを付与しています" mb="2">
                         処理中...
                     </ProgressLabel>
-                    <ProgressBar />
+                    <HStack gap="5">
+                        <ProgressBar flex="1" />
+                        <ProgressValueText>{processedCount}%</ProgressValueText>
+                    </HStack>
                 </ProgressRoot>
             </Center>
         </>
