d6e4a8ca884f36e3a412521bec48d6a50ef976b9
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-30 01:01:30 +0900

    アバターと画像のパスをユーザーホームディレクトリからCドライブのProgramDataに変更

--- app.go
+++ app.go
@@ -24,8 +24,8 @@ var (
 	currentDirectory, _ = os.Getwd()
 	configJson = filepath.Join(currentDirectory, "Config", "config.json")
 	UserHomeDir, _ = os.UserHomeDir()
-	AvatarsPath = filepath.Join(UserHomeDir, "AppData", "Local", "VRC-Avatar-Library", "Avatars")
-	ImagesPath = filepath.Join(UserHomeDir, "AppData", "Local", "VRC-Avatar-Library", "Images")
+	AvatarsPath = filepath.Join("C:", "ProgramData", "VRC-Avatar-Library", "Avatars")
+	ImagesPath = filepath.Join("C:", "ProgramData", "VRC-Avatar-Library", "Images")
 )
 
 // NewApp creates a new App application struct
